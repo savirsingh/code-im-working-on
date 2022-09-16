13fdd99f2984d863af9aa83213a6ebe19309023a
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-09-16 17:36:43 -0400

    Update ecoo19r1p1.cpp

--- ecoo19r1p1.cpp
+++ ecoo19r1p1.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-// macros
 #define int long long
 #define del erase
 #define pb push_back
@@ -7,38 +6,26 @@ using namespace std;
 
 int32_t main()
 {
-    // go through 10 times
     for (int z=0; z<10; z++) {
-        // map to hold all the event days
-        map<int, int> events;
-        // all variables (initial and dirty are not used)
-        int n, m, d, initial, dirty=0;
+        int n, m, d;
         cin >> n >> m >> d;
+        vector<int> event_days (m);
         vector<int> laundry_days;
-        int total_shirts=n;
-        // get all the events input
+        int total_shirts = n;
         for (int i=0; i<m; i++) {
-            int a;
-            cin >> a;
-            if (events.find(a)==events.end()) {
-                events[a]=1;
-            }
-            else {
-                events[a]++;
-            }
+            cin >> event_days[i];
         }
-        // actually working through the d days
         for (int j=1; j<=d; j++) {
-            if (n<1) {
+            if (n==0) {
                 n=total_shirts;
                 laundry_days.pb(j);
             }
-            if (events.find(j)!=events.end()) {
-                total_shirts+=events[j];
+            if (find(event_days.begin(), event_days.end(), j)!=event_days.end()) {
+                total_shirts++;
+                n++;
             }
             n--;
         }
-        // outputting number of laundry days
         cout << laundry_days.size() << "\n";
     }
 }
