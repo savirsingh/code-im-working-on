99a3dac54f97999ec2a71c43b902c9572fc9779c
Savir Singh <84334654+savirsingh@users.noreply.github.com>
2022-09-23 12:00:47 -0400

    Create ac19p1.cpp

---
+++ ac19p1.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    for (int i=0; i<t; i++) {
+        int n, a, b, c;
+        cin >> n >> a >> b >> c;
+        int total=0;
+        if (c>=n) {
+            cout << 0 << " " << 0 << " " << n;
+        }
+    }
+}
